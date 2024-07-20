5f5003806d7e7ebf9b78d403e7df5fab668916c3
Anthony <athompson@posteo.net>
2024-07-20 16:25:08 +1000

    changed nixos and home-manager to unstable

--- hm/flake.nix
+++ hm/flake.nix
@@ -2,10 +2,10 @@
   description = "Home Manager configuration of anthony";
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,7 +2,7 @@
 	description = "My flake.nix";
 
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 	};
 
 	outputs = { self, nixpkgs, ... } @inputs: {
